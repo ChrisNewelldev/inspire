fb3c2f976306b36ff79f337726c5c4df72ea9829
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-24 23:38:18 -0600

    not ready yet

---
+++ app/Models/Todo.js
@@ -0,0 +1,19 @@
+export default class Todo {
+  constructor(data) {
+    this._id = data._id,
+      this.completed = data.completed,
+      this.description = data.description
+    this.user = data.user
+  }
+
+  get TodoTemplate() {
+    `
+    
+    
+    `
+
+
+  }
+
+}
+
