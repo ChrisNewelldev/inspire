4e16d29d5712fe683ee807422cec9414189e94f6
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-26 15:55:47 -0600

    working on forms

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -1,20 +1,28 @@
 export default class Todo {
   constructor(data) {
-    this.id = data.id,
-      this.completed = data.completed,
-      this.description = data.description,
-      this.user = data.user
+
+
+    this.id = data.id
+    this.completed = data.completed
+
+    this.user = data.user
+
+    this.description = data.description
   }
+
 
   get Template() {
     return `
-    <li>${this.description}</li>
-    <button class="btn btn-danger" onclick="app.todosController.deleteTodo('${this.id}')"> delete </button>
+    <form class="list">
+                    <div class="form-check">
+                        <input class="form-check-input position-static" type="checkbox" id="blankCheckbox"
+                            value="option1" aria-label="...">
+                    </div>${data.description}list<button class="btn"
+                        onclick="app.todosController.deleteTodo('${data.id}')">
+                        delete </button>
+                    <input type="text" placeholder="Write a to do.." />
+                    <button type="submit" class="btn">Add Todo</button>
+                </form>
     `
-
-
   }
-
-
-
 }
